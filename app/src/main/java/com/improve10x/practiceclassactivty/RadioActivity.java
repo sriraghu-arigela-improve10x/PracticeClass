5cdafdd81bf09f52b8a0cd750c61890cc8c492bf
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-01-25 14:45:37 +0530

    created new activity RadioActivity

---
+++ app/src/main/java/com/improve10x/practiceclassactivty/RadioActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.practiceclassactivty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RadioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio);
+    }
+}

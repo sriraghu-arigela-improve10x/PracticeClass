b4a97dc0c955684d2cf2526b0d7d6286b0b8531d
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-01-29 22:41:32 +0530

    created new Activity SpinnerActivity and implementation,XML design

--- app/src/main/java/com/improve10x/practiceclassactivty/SpinnerActivity.java
+++ app/src/main/java/com/improve10x/practiceclassactivty/SpinnerActivity.java
@@ -3,22 +3,54 @@ package com.improve10x.practiceclassactivty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.improve10x.practiceclassactivty.databinding.ActivitySpinnerBinding;
+
+import java.util.ArrayList;
 
 public class SpinnerActivity extends AppCompatActivity {
 
     private ActivitySpinnerBinding binding;
-
+    private ArrayList<String> countries;
+    private Spinner countrySp;
+    private ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySpinnerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Spinner");
+        setData();
         handleSpinner();
     }
 
+    private void setData() {
+        countries = new ArrayList<>();
+        countries.add("India");
+        countries.add("Alaska");
+        countries.add("USA");
+        countries.add("Ukraine");
+        countries.add("Japan");
+    }
+
     private void handleSpinner() {
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        binding.spinner.setAdapter(arrayAdapter);
+        binding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(SpinnerActivity.this, countries.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
